303ded2db2006520961a25b913331adc7e1bfb87
root4loot <mail@root4loot.com>
2019-03-30 20:30:54 +0100

    Add func to get file from project root

--- pkg/io/io.go
+++ pkg/io/io.go
@@ -11,6 +11,10 @@ import (
 	"bufio"
 	"io/ioutil"
 	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 // IsFileExist returns bool
@@ -46,4 +50,30 @@ func CreateFile(fn string) (*os.File, error) {
 func WriteFile(f *os.File, data []byte) (int, error) {
 	w, err := f.Write(data)
 	return w, err
+}
+
+// ReadFileFromProjectRoot opens, read and returns a file from project root dir
+func ReadFileFromProjectRoot(file, trim string) (fr []byte) {
+	// runtime caller
+	_, caller, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("No caller information")
+	}
+
+	// path to caller (project/dir/package/package.go)
+	caller = path.Dir(caller)
+	// path to caller parent dir (project/dir)
+	callerDir := filepath.Dir(caller)
+	// trim dir to get the project root (project)
+	projectRoot := strings.TrimRight(callerDir, trim)
+	// get file from project root (project/file)
+	file = projectRoot + file
+
+	// open and read
+	fo, _ := OpenFile(file)
+	fr, _ = ReadFile(fo)
+	// close
+	defer fo.Close()
+
+	return fr
 }
