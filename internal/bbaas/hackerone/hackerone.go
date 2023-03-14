fb3af15639a241a36fd7099d6a59b59fba553e3f
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-03-14 21:14:33 +0100

    fix #13

--- internal/bbaas/hackerone/hackerone.go
+++ internal/bbaas/hackerone/hackerone.go
@@ -8,6 +8,7 @@ package hackerone
 import (
 	"bytes"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"reflect"
@@ -20,7 +21,6 @@ import (
 )
 
 var scope []string
-var isAssetURL bool
 
 // Scrape tries to grab scope table for a given program on hackerone.com
 func Scrape(url string) string {
@@ -32,9 +32,9 @@ func Scrape(url string) string {
 	// clear global slice
 	scope = nil
 
-	var data = []byte(`{  
+	var data = []byte(`{
 		"query":"query Team_assets($first_0:Int!) {query {id,...F0}} fragment F0 on Query {_teamAgUhl:team(handle:\"` + program + `\") {handle,_structured_scope_versions2ZWKHQ:structured_scope_versions(archived:false) {max_updated_at},_structured_scopeszxYtW:structured_scopes(first:$first_0,archived:false,eligible_for_submission:true) {edges {node {asset_type, asset_identifier}},pageInfo {hasNextPage,hasPreviousPage}},_structured_scopes3FF98f:structured_scopes(first:$first_0,archived:false,eligible_for_submission:false) {edges {node {asset_type,asset_identifier,},},},},}",
-		"variables":{  
+		"variables":{
 		   "first_0":1337
 		}
 	 }`)
@@ -45,7 +45,9 @@ func Scrape(url string) string {
 		doerror.NoScope(url)
 	}
 
-	resB, _ := (Post(endpoint, data))
+	hostsession, csrf := GetSession()
+
+	resB, _ := (Post(endpoint, data, hostsession, csrf))
 	resS := BytesToString(resB)
 
 	re = regexp.MustCompile(`\"edges":\[(.*?)\]`)
@@ -75,15 +77,40 @@ func BytesToString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&sh))
 }
 
+func GetSession() (hostsession, csrf string) {
+	resp, err := http.Get("https://hackerone.com/security")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	cookies := resp.Header["Set-Cookie"]
+	for _, cookie := range cookies {
+		if strings.HasPrefix(cookie, "__Host-session") {
+			hostsession = strings.Split(cookie, ";")[0]
+		}
+	}
+
+	r := regexp.MustCompile(`<meta name="csrf-token" content="([\w+\/=]+)`)
+	m := r.FindStringSubmatch(string(body))
+	csrf = m[1]
+
+	return hostsession, csrf
+}
+
 // Post makes a post request with custom X-Auth-Token header
-func Post(url string, data []byte) ([]byte, int) {
+func Post(url string, data []byte, hostsession, csrf string) ([]byte, int) {
 	token := os.Getenv("H1_TOKEN")
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(data))
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Connection", "close")
 	req.Header.Set("X-Auth-Token", token)
-	req.Header.Set("Cookie", "__Host-session=dzQzc28yMlFrL2tsNmVGUmE5bFR0RC9qK2NsU3pmVEdkb0NyVmtjMU85V25kTUZkUHcvVFp5VEdSS1N1RUxrSjlzZjY2VlF0NzNSUlpNWUZsK1VpSjhqK3lsVVR1VkVCMFNQbTZDSUJMYmVQYjB3T2xqNUVkTVI4NE9PNHlEdXhxeXZPTFNObHJJM2lzdFRYOFZrRjJaYXBFMUZKelFLZVB2WU9uYTlkbG9GNm0rVnNWajhzOTBuKzF3ekViOS9KZzhzVjlvY3lEVmhEVDROV091WHV0TXhiVGdScTBRR2xYNlhhU3lpeEtLNlNrSS85R2YvaE95YjVTYnNWU0RLNmFlWFExenVQaGl1ODhzNmNFK2E1bUJNT054RU8ySHRUZmcrVS9aVHIvTkE9LS05TE9qNSt6TW9ocnNYaGZNUnRhTU5nPT0%3D--baa089405ea2c9c72ce46c8c7d78940e4eefb40e;")
-	req.Header.Set("X-Csrf-Token", "dzO0nLmnGT+YirJwGeXSm2OLGfs2cCIqvTahjX5nAEb9CyDWaByMXdNAVaTCBT/XTuZODcumKuj/973Ytbw/Qg==")
+	req.Header.Set("Cookie", hostsession)
+	req.Header.Set("X-Csrf-Token", csrf)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
